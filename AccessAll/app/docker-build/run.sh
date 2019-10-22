f85150aeb4ed981f9da7379ece22101fba63baf0
joangui <joan@ac.upc.edu>
2019-10-22 19:44:12 +0200

    reduce blank space

--- AccessAll/app/docker-build/run.sh
+++ AccessAll/app/docker-build/run.sh
@@ -10,13 +10,6 @@ chmod -R +x /opt/ConfigurationScripts/*.sh
 /opt/ConfigurationScripts/waitForMySQL.sh
 
 #CONNECTORS
-
-
-
-echo " /opt/ConfigurationScripts/setJDBCSinkConnector.sh -c "cigo-jdbc-sink_AccessAll" -k $KAFKA_CONNECT_HOST -s $SCHEMA_REGISTRY_HOST_NAME -n $SINK_TOPICS -m $MYSQL_HOST -d $MYSQL_DATABASE -u $MYSQL_USER -p $MYSQL_PASSWORD -pk \"mytrac_id\" -ac \"true\""
-
-
-
 /opt/ConfigurationScripts/setJDBCSinkConnector.sh\
     -c "cigo-jdbc-sink_AccessAll"\
     -k $KAFKA_CONNECT_HOST\
